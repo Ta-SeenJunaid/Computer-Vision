cac83180e833726a642eb335135ebc8c186cb166
Ta-SeenJunaid <taseen.junaid@gmail.com>
2019-02-18 19:08:24 +0600

    6 is added

---
+++ 6/4.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+# -*- coding: utf-8 -*-
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+flat_chess = cv2.imread('DATA/flat_chessboard.png')
+flat_chess = cv2.cvtColor(flat_chess,cv2.COLOR_BGR2RGB)
+
+#plt.imshow(flat_chess)
+
+gray_flat_chess = cv2.cvtColor(flat_chess,cv2.COLOR_BGR2GRAY)
+#plt.imshow(gray_flat_chess,cmap='gray')
+
+real_chess = cv2.imread('DATA/real_chessboard.jpg')
+real_chess = cv2.cvtColor(real_chess,cv2.COLOR_BGR2RGB)
+
+#plt.imshow(real_chess)
+gray_flat_chess = cv2.cvtColor(flat_chess,cv2.COLOR_BGR2GRAY)
+gray_real_chess = cv2.cvtColor(real_chess,cv2.COLOR_BGR2GRAY)
+#plt.imshow(gray_real_chess,cmap='gray')
+
+corners = cv2.goodFeaturesToTrack(gray_flat_chess,5,0.01,10)
+corners = np.int0(corners)
+
+for i in corners:
+    x,y = i.ravel()
+    cv2.circle(flat_chess,(x,y),3,(255,0,0),-1)
+
+plt.imshow(flat_chess)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
