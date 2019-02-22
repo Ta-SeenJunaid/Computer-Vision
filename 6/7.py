a51e023a8cd400e58e4e694f83b3bb57ab9b751e
Ta-SeenJunaid <taseen.junaid@gmail.com>
2019-02-23 04:22:19 +0600

    contour

--- 6/7.py
+++ 6/7.py
@@ -11,5 +11,31 @@ image,contours,hierarchy = cv2.findContours(img,cv2.RETR_CCOMP,cv2.CHAIN_APPROX_
 
 external_contours = np.zeros(image.shape)
 
+#a = hierarchy 
+#b= contours
+for i in range(len(contours)):
+    
+    if hierarchy[0][i][3] == -1:
+        
+        cv2.drawContours(external_contours,contours,i,255,-1)
+    
+plt.imshow(external_contours,cmap='gray')    
+   
+internal_contours = np.zeros(image.shape)
+
+#a = hierarchy 
+#b= contours
+for i in range(len(contours)):
+    
+    if hierarchy[0][i][3] != -1:
+        
+        cv2.drawContours(internal_contours,contours,i,255,-1)
+    
+plt.imshow(internal_contours,cmap='gray')  
 
 
+
+
+
+
+ 
