ffac0eb65319d1b72b596865ff165248e1de24ae
Ta-SeenJunaid <taseen.junaid@gmail.com>
2019-02-26 20:39:59 +0600

    Segmentaion

--- 6/11.py
+++ 6/11.py
@@ -12,9 +12,10 @@ def display(img,cmap='gray'):
     ax.imshow(img,cmap='gray')
     
 img = cv2.imread('DATA/pennies.jpg')
+sep_coins = img
 #display(img)
 
-img = cv2.medianBlur(sep_coins,35)
+img = cv2.medianBlur(img,35)
 display(img)
 
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
@@ -28,6 +29,7 @@ opening = cv2.morphologyEx(thresh,cv2.MORPH_OPEN,kernel,iterations=2)
 display(opening)
 
 sure_bg = cv2.dilate(opening,kernel,iterations=3)
+#This is as same as opening
 display(sure_bg)
  
 dist_transform = cv2.distanceTransform(opening,cv2.DIST_L2,5)
@@ -41,8 +43,22 @@ unknown = cv2.subtract(sure_bg,sure_fg)
 display(unknown)
 
 ret , markers = cv2.connectedComponents(sure_fg)
-
 markers = markers +1
-markers[]
+markers[unknown==255] = 0
+display(markers)
+
+markers = cv2.watershed(img,markers)
+display(markers)
+
+image,contours,hierarchy = cv2.findContours(markers.copy(),cv2.RETR_CCOMP,cv2.CHAIN_APPROX_SIMPLE)
+
+for i in range(len(contours)):
+    
+    if hierarchy[0][i][3] == -1:
+        cv2.drawContours(sep_coins,contours,i,(255,0,0),10)
+
+display(sep_coins)
+
+
 
 
---
+++ 6/12.py
@@ -0,0 +1,81 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+road = cv2.imread('DATA/road_image.jpg')
+road_copy = np.copy(road)
+#plt.imshow(road)
+#road.shape[:2]
+marker_image = np.zeros(road.shape[:2],dtype=np.int32)
+segments = np.zeros(road.shape,dtype=np.uint8)
+
+from matplotlib import cm
+#cm.tab10(0)
+
+def create_rgb(i):
+    return tuple(np.array(cm.tab10(i)[:3])*255)
+
+colors = []
+for i in range(10):
+    colors.append(create_rgb(i))
+
+###
+#global varibles
+n_markers = 10
+current_marker = 1
+marks_updated = False
+#callback function
+def mouse_callback(event,x,y,flags,param):
+    global marks_updated
+    
+    if event == cv2.EVENT_LBUTTONDOWN:
+        cv2.circle(marker_image,(x,y),10,(current_marker),-1)
+        
+        cv2.circle(road_copy,(x,y),10,colors[current_marker],-1)
+        
+        marks_updated = True
+    
+#while true    
+cv2.namedWindow('Road Image')
+cv2.setMouseCallback('Road Image', mouse_callback) 
+
+while True:
+    
+    cv2.imshow('Watershed Segmens',segments)
+    cv2.imshow('Road Image',road_copy)
+    
+    k = cv2.waitKey(1)
+    
+    if k == 27:
+        break
+    
+    elif k==ord('c'):
+        road_copy = road.copy()
+        marker_image = np.zeros(road.shape[:2],dtype=np.int32)
+        segments = np.zeros(road.shape,dtype=np.uint8)
+        
+    elif k > 0 and chr(k).isdigit():
+        current_marker = int(chr(k))
+        
+    if marks_updated:
+        
+        marker_image_copy = marker_image.copy()
+        cv2.watershed(road,marker_image_copy)
+        
+        segments = np.zeros(road.shape,dtype=np.uint8)
+        
+        for color_ind in range(n_markers):
+            
+            segments[marker_image_copy==(color_ind)] = colors[color_ind]
+  
+
+  
+    
+cv2.destroyAllWindows()
+    
+    
+    
+    
+    
+    
+    
