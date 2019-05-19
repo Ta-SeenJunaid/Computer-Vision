b2fa8ca2094d49cae4ccf72932653a017e11976b
Ta-SeenJunaid <taseen.junaid@gmail.com>
2019-05-19 10:10:33 +0600

    Finger Counting with Convex Hull

--- finger tracking/1.py
+++ finger tracking/1.py
@@ -38,12 +38,62 @@ def segment(frame,threshold_min=25):
         
         hand_segment = max(contours,key=cv2.contourArea)
         
-        return (tresholded,hand_segment)
+        return (thresholded,hand_segment)
+    
+    
+ def count_fingers(thresholded,hand_segment):
+     
+     conv_hull = cv2.convexHull(hand_segment)
+     
+     top = tuple(conv_hull[conv_hull[;,;,1].argmin()[0]])
+     bottom = tuple(conv_hull[conv_hull[;,;,1].argmax()[0]])
+     left = tuple(conv_hull[conv_hull[;,;,1].argmin()[0]])
+     right = tuple(conv_hull[conv_hull[;,;,1].argmax()[0]])
+     
+     
+     cX = (left[0] + right[0]) // 2
+     cY = (top[1] + bottom[1]) // 2
+    
+    distance = pairwise.euclidean_distances([cX,cY],Y=[left,right,top,bottom])[0]
+    
+    max_distance = distance.max()
+    
+    radius = int(0.9*max_distance)
+    circumfrence = (2*np.pi*radius)
+    
+    
+    circular_roi = np.zeros(thresholded[:2],dtype='uint8')
+    
+    cv2.circle(circular_roi,(cX,cY),radius,255,10)
+    
+    circular_roi = cv2.bitwise_and(thresholded, thresholded, mask=circular_roi)
+    
+    image,contours,hierarchy = cv2.findContours(circular_roi.copy(),cv2.RETR_EXTERNAL,CHAAIN_APPROX_NONE)
+    
+    
+    count = 0
+    
+    for cnt in contours:
+        
+        (x,y,w,h) = cv2.boundingRect(cnt)
+        
+        out_of_wrist = (cY + (cY*0.25)) > (y+h)
+        
+        limit_points = ((circumfrence*0.25) > cnt.shape[0])
+        
+        if out_of_wrist and limit_points:
+            count +=1
+            
+    return count
     
     
     
     
     
+        
+        
+        
+        
 
 
 
