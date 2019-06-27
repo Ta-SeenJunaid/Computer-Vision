2267841ab78ddd898ea4918c0ce535c35ea9095e
Ta-SeenJunaid <taseen.junaid@gmail.com>
2019-06-27 22:34:28 +0600

    cascades loading

--- face detection/face_detection.py
+++ face detection/face_detection.py
@@ -1,2 +1,5 @@
 
 import cv2
+
+face_cascade = cv2.CascadeClassifier('../data2/haar-cascade-files/haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('../data2/haar-cascade-files/haarcascade_eye.xml')
