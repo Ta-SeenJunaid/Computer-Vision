6ff6200601c06f154dc41fba08bdfa1566286f57
Ta-SeenJunaid <taseen.junaid@gmail.com>
2019-06-28 06:24:28 +0600

    loop to track eyes and completion of detect function

--- face detection/face_detection.py
+++ face detection/face_detection.py
@@ -7,8 +7,12 @@ eye_cascade = cv2.CascadeClassifier('../data2/haar-cascade-files/haarcascade_eye
 def detect(gray, frame):
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = frame[y:y+h, x:x+w]
         eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 3)
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (255, 0, 0), 2)
+    return frame
+
         
