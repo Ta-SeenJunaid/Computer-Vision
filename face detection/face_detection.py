8fbd4d77c5008e31b1ab4537d39f69043b9f1f9e
Ta-SeenJunaid <taseen.junaid@gmail.com>
2019-06-29 21:35:37 +0600

    video_capture and release

--- face detection/face_detection.py
+++ face detection/face_detection.py
@@ -19,6 +19,13 @@ video_capture = cv2.VideoCapture(0)
 while True:
     _, frame = video_capture.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    canvas = detect(gray, frame)
+    cv2.imshow('face detection', canvas)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+video_capture.release()
+cv2.destroyAllWindows()
+    
     
 
 
