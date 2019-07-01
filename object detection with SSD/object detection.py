2bd28284445e0d2ba655b30e958ac2d2dbb4ebd7
Ta-SeenJunaid <taseen.junaid@gmail.com>
2019-07-01 22:15:53 +0600

    detect function

--- object detection with SSD/object detection.py
+++ object detection with SSD/object detection.py
@@ -1,2 +1,13 @@
-# -*- coding: utf-8 -*-
 
+import torch
+from torch.autograd import Variable
+import cv2
+from data import BaseTransform, VOC_CLASSES as labelmap
+from ssd import build_ssd
+import imageio
+
+def detect(frame, net, transform):
+    height, width = frame.shape[:2]
+    frame_t = transform(frame)[0]
+    x = torch.from_numpy(frame_t).permute(2, 0, 1)
+    x = Variable(x.unsqueeze(0))
