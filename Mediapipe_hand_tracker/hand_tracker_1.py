53bdee7b43590cd4dd823fdd5646a516e1d30d0b
Ta-SeenJunaid <taseen.junaid@gmail.com>
2021-04-21 08:11:59 +0600

    hand_tracker_1.py

---
+++ Mediapipe_hand_tracker/hand_tracker_1.py
@@ -0,0 +1,22 @@
+import cv2
+import mediapipe as mp
+
+cap = cv2.VideoCapture(0)
+
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands()
+mp_draw = mp.solutions.drawing_utils
+
+while True:
+    success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+
+    if results.multi_hand_landmarks:
+        for hand_lms in results.multi_hand_landmarks:
+            mp_draw.draw_landmarks(img, hand_lms, mp_hands.HAND_CONNECTIONS)
+
+    cv2.namedWindow("Image", cv2.WINDOW_NORMAL)
+    cv2.resizeWindow("Image", 1000, 1000)
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
