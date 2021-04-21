a933cec4e4248fa64faf886107fd1542dfa1694e
Ta-SeenJunaid <taseen.junaid@gmail.com>
2021-04-21 12:30:39 +0600

    hand_tracker_1.py update hand point  & frame count

--- Mediapipe_hand_tracker/hand_tracker_1.py
+++ Mediapipe_hand_tracker/hand_tracker_1.py
@@ -1,11 +1,15 @@
 import cv2
 import mediapipe as mp
+import time
 
 cap = cv2.VideoCapture(0)
 
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands()
 mp_draw = mp.solutions.drawing_utils
+
+p_time = 0
+c_time = 0
 
 while True:
     success, img = cap.read()
@@ -14,7 +18,24 @@ while True:
 
     if results.multi_hand_landmarks:
         for hand_lms in results.multi_hand_landmarks:
+            for id, lm in enumerate(hand_lms.landmark):
+                print(id, lm)
+                h, w, c = img.shape
+                cx, cy = int(lm.x*w), int(lm.y*h)
+                print(id, cx, cy)
+                if id == 4:
+                    cv2.circle(img, (cx, cy), 8, (255, 0, 255), cv2.FILLED)
+                if id == 8:
+                    cv2.circle(img, (cx, cy), 8, (255, 125, 155), cv2.FILLED)
+
             mp_draw.draw_landmarks(img, hand_lms, mp_hands.HAND_CONNECTIONS)
+
+    c_time = time.time()
+    fps = 1/(c_time-p_time)
+    p_time = c_time
+
+    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3,
+                (255, 0, 255), 3)
 
     cv2.namedWindow("Image", cv2.WINDOW_NORMAL)
     cv2.resizeWindow("Image", 1000, 1000)
