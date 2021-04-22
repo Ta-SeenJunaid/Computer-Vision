546f2968df1ac26e87d9b26acd19146dce1b3389
Ta-SeenJunaid <taseen.junaid@gmail.com>
2021-04-22 19:59:38 +0600

    Find position

--- Mediapipe_hand_tracker/hand_detection_and_tracking.py
+++ Mediapipe_hand_tracker/hand_detection_and_tracking.py
@@ -15,18 +15,37 @@ class HandDetectionAndTracking():
         self.hands = self.mp_hands.Hands(self.mode, self.max_hands,
                                          self.detection_con, self.track_con)
         self.mp_draw = mp.solutions.drawing_utils
+        self.results = None
 
     def find_hands(self, img, draw=True):
-        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        results = self.hands.process(imgRGB)
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(img_rgb)
 
-        if results.multi_hand_landmarks:
-            for hand_lms in results.multi_hand_landmarks:
+        if self.results.multi_hand_landmarks:
+            for hand_lms in self.results.multi_hand_landmarks:
                 if draw:
                     self.mp_draw.draw_landmarks(img, hand_lms,
                                                 self.mp_hands.HAND_CONNECTIONS)
 
         return img
+
+    def find_position(self, img, hand_no=0, draw = True):
+
+        lm_list = []
+        if self.results.multi_hand_landmarks:
+            my_hand = self.results.multi_hand_landmarks[hand_no]
+
+            for id, lm in enumerate(my_hand.landmark):
+                print(id, lm)
+                h, w, c = img.shape
+                cx, cy = int(lm.x*w), int(lm.y*h)
+                lm_list.append([id, cx, cy])
+                if draw:
+                    cv2.circle(img, (cx, cy), 10, (255, 153, 255), cv2.FILLED)
+                # if id == 8:
+                #     cv2.circle(img, (cx, cy), 10, (255, 128, 0), cv2.FILLED)
+
+        return lm_list
 
 
 def main():
@@ -38,6 +57,9 @@ def main():
     while True:
         success, img = cap.read()
         img = detector.find_hands(img)
+        lm_list = detector.find_position(img)
+        if len(lm_list) != 0:
+            print(lm_list[4])
 
         c_time = time.time()
         fps = 1 / (c_time - p_time)
