ac58b38a452750bdfb53c26316f75f68d9c24f6e
Ta-SeenJunaid <taseen.junaid@gmail.com>
2021-05-07 22:33:34 +0600

    save into csv file

--- Face recognition and attendance monitoring/face_recognition_and_attendance_monitoring_2.py
+++ Face recognition and attendance monitoring/face_recognition_and_attendance_monitoring_2.py
@@ -2,17 +2,19 @@ import cv2
 import numpy as np
 import face_recognition
 import os
+from datetime import datetime
 
 path = 'image_attendance'
 images = []
 class_names = []
 my_list = os.listdir(path)
-print(my_list)
+# print(my_list)
 for cl in my_list:
     cur_img = cv2.imread(f'{path}/{cl}')
     images.append(cur_img)
     class_names.append(os.path.splitext(cl)[0])
 print(class_names)
+
 
 def find_encodings(images):
     encode_list = []
@@ -23,6 +25,19 @@ def find_encodings(images):
     return encode_list
 
 
+def mark_attendance(name):
+    with open('attendance.csv', 'r+') as f:
+        my_data_list = f.readlines()
+        name_list = []
+        for line in my_data_list:
+            entry = line.split(',')
+            name_list.append(entry[0])
+        if name not in name_list:
+            now = datetime.now()
+            dt_string = now.strftime('%H:%M:%S')
+            f.writelines(f'\n{name},{dt_string}')
+
+
 encode_list_known = find_encodings(images)
 print('Encoding Complete')
 
@@ -30,6 +45,8 @@ cap = cv2.VideoCapture(0)
 
 while True:
     success, img = cap.read()
+    img = cv2.flip(img, 1)
+
     img_small = cv2.resize(img, (0,0), None, 0.25, 0.25)
     img_small = cv2.cvtColor(img_small, cv2.COLOR_BGR2RGB)
 
@@ -39,17 +56,21 @@ while True:
     for encode_face, face_loc in zip(encodes_current_frame, faces_current_frame):
         matches = face_recognition.compare_faces(encode_list_known, encode_face)
         face_dis = face_recognition.face_distance(encode_list_known, encode_face)
-        print(face_dis)
+        # print(face_dis)
         match_index = np.argmin(face_dis)
 
         if matches[match_index]:
             name = class_names[match_index].upper()
-            print(name)
+            # print(name)
             y1, x2, y2, x1 = face_loc
             y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
             cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
             cv2.rectangle(img, (x1, y2-35), (x2, y2), (0, 255, 0), cv2.FILLED)
             cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+            mark_attendance(name)
 
     cv2.imshow('webcam',img)
-    cv2.waitKey(1)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cv2.destroyAllWindows()
