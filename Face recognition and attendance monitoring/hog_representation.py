f2674c38b610d6d4dc816cb8ba1b89c8e1cd5e96
Ta-SeenJunaid <taseen.junaid@gmail.com>
2022-01-27 19:25:32 +0100

    hog_representation.py

---
+++ Face recognition and attendance monitoring/hog_representation.py
@@ -0,0 +1,30 @@
+# link: https://scikit-image.org/docs/dev/auto_examples/features_detection/plot_hog.html#sphx-glr-auto-examples-features-detection-plot-hog-py
+
+import cv2
+import matplotlib.pyplot as plt
+from skimage.feature import hog
+from skimage import data, exposure
+
+
+filename = 'image_attendance/Roushan.JPG'
+im = cv2.imread(filename)
+# gr = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+
+image = im
+
+fd, hog_image = hog(image, orientations=8, pixels_per_cell=(16, 16),
+                    cells_per_block=(1, 1), visualize=True, channel_axis=-1)
+
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 4), sharex=True, sharey=True)
+
+ax1.axis('off')
+ax1.imshow(image, cmap=plt.cm.gray)
+ax1.set_title('Input image')
+
+# Rescale histogram for better display
+hog_image_rescaled = exposure.rescale_intensity(hog_image, in_range=(0, 10))
+
+ax2.axis('off')
+ax2.imshow(hog_image_rescaled, cmap=plt.cm.gray)
+ax2.set_title('Histogram of Oriented Gradients')
+plt.show()
